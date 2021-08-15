d5824ae0f8b9e5a3eff5e7b3ddf1fbb25217b4c5
peijia97 <peijia97@hotmail.com>
2021-08-15 18:40:40 +0800

    code cleanup

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,9 @@
 import React from "react";
 
 const HomePage = React.lazy(() => import("./views/home/HomePage"));
-const PreviewPage = React.lazy(() => import("./views/preview/PreviewPage"));
 
 const routes = [
   { path: "/", exact: true, name: "Home" },
-  { path: "/preview", name: "PreviewPage", component: PreviewPage },
   { path: "/", name: "HomePage", component: HomePage }
 ];
 
